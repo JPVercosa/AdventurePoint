54af0a327b0ef534fba496b36fd1e107b5a17326
João Verçosa <jpkqvercosa@hotmail.com>
2021-07-10 13:31:56 -0300

    Bootstraped Index, pagina com todos os campos

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const rand = Math.floor(Math.random() * 474);
+        const price = Math.floor(Math.random() * 200 + 50);
         const newCamp = new Campground({
             location: `${cities[rand].city}, ${cities[rand].admin_name}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/829248',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus obcaecati, pariatur praesentium id ducimus odit architecto cum hic! Fuga, temporibus. Necessitatibus cumque sapiente odio sed cupiditate similique qui nihil tempore.',
+            price
         })
         await newCamp.save();
     }
