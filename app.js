25ef77fa54f22ba2f1d910e5b909f3902bc343b8
João Verçosa <jpkqvercosa@hotmail.com>
2022-01-14 10:26:23 -0300

    Pasta utils e error handling (back-end)
    Continuação da separação do projeto em pedaços menores, com criação da pasta de utilidades e capturando os erros de execução, e possíveis erros relacionados ao CRUD

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ const mongoose = require('mongoose');                   //Comunicação com o ba
 const methodOverride = require('method-override');      //Possibilita utilizar métodos além de GET e POST
 const morgan = require('morgan');                       //Imprime logs de rede no console após conexões [MIDDLEWARE]
 const ejsMate = require('ejs-mate');                    //Permite a adição de Layouts
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError')
+const { campgroundSchema } = require('./utils/schemas')
+
 
 const Campground = require('./models/campground');
 const { request } = require('express');
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body)
+    if (error) {
+        const message = error.details.map(el => el.message).join(', ');
+        throw new ExpressError(message, 400);
+    } else {
+        next()
+    }
+}
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => {
@@ -32,50 +47,55 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const allCampgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds: allCampgrounds });
-})
+}))
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+
     const newCamp = new Campground(req.body.campground);  //Pegando as informações que vieram via POST de "wwww.../campground/new"
     await newCamp.save();                                 //Salvando o novo modelo de Campground no Mongo
     res.redirect(`/campgrounds/${newCamp._id}`)
-})
+}))
 
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findById(id);
     res.render('campgrounds/show', { campground })
-})
+}))
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit', { campground })
+}))
 
-})
-
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params
     const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     res.redirect(`/campgrounds/${id}`)
-})
+}))
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404))
 })
 
 app.use((err, req, res, next) => {
-    console.log(err)
-    res.status(404).send("404 - Page not Found")
+    const { status = 500 } = err;
+    if (!err.message) err.message = "Oh no, something went wrong"
+    res.status(status).render('error', { err })
 })
 
 app.listen(3100, () => {
