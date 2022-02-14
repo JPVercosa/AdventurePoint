d3e4b835989815b8454e8db28d88b60cc374d2e9
João Verçosa <jpkqvercosa@hotmail.com>
2022-02-13 21:31:05 -0300

    Adding MapBox Basic

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -2,6 +2,8 @@ const campground = require('../models/campground');
 const Campground = require('../models/campground');
 
 const { cloudinary } = require("../cloudinary/cloudStorage");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding")
+const geocoder = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN })
 
 module.exports.index = async (req, res) => {
     const allCampgrounds = await Campground.find({});
@@ -13,10 +15,15 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res) => {
-    const newCamp = new Campground(req.body.campground);  //Pegando as informações que vieram via POST de "wwww.../campground/new"
-    newCamp.images = req.files.map(file => ({ url: file.path, filename: file.filename }))  //Pegando as imagens com Muter+Cloudinary
-    newCamp.author = req.user._id;                        //Adicionando a informação de quem é o autor
-    await newCamp.save();                                 //Salvando o novo modelo de Campground no Mongo
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
+    const newCamp = new Campground(req.body.campground);                                    //Pegando as informações que vieram via POST de "wwww.../campground/new"
+    newCamp.geometry = geoData.body.features[0].geometry;                                   //Adicionando os dados do GeoJSON no Campground e salvando no Mongo
+    newCamp.images = req.files.map(file => ({ url: file.path, filename: file.filename }))   //Pegando as imagens com Muter+Cloudinary
+    newCamp.author = req.user._id;                                                          //Adicionando a informação de quem é o autor
+    await newCamp.save();                                                                   //Salvando o novo modelo de Campground no Mongo
     console.log(newCamp)
     req.flash('success', 'Novo Ponto criado!')
     res.redirect(`/campgrounds/${newCamp._id}`)
